7fc51016bfd9c8c3768cf9f8c8be91f9e28e64f1
jhz <1728533387@qq.com>
2023-04-11 00:25:25 +0800

    main-初始化
    main-初始化

---
+++ gupaoedu-vip-mall/mall-service/mall-goods-service/src/main/java/com/gupaoedu/vip/mall/goods/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package com.gupaoedu.vip.mall.goods.controller;
+
+import com.gupaoedu.mall.util.RespResult;
+import com.gupaoedu.vip.mall.goods.model.Category;
+import com.gupaoedu.vip.mall.goods.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/*****
+ * @Author: 波波
+ * @Description: 云商城
+ ****/
+@RestController
+@RequestMapping(value = "/category")
+@CrossOrigin
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /****
+     * 根据分类父ID查询子分类
+     */
+    @GetMapping(value = "/parent/{id}")
+    public RespResult<List<Category>> findByParentId(@PathVariable("id")Integer id){
+        return RespResult.ok(categoryService.findByParentId(id));
+    }
+
+}
